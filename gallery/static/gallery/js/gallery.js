54b714a03a749610f091755ba2941410f919751b
Matt McCarthy <matt@mccarthywebdesign.com>
2020-06-05 13:07:38 -0500

    Gallery app styling changes
    Fixed strange button positioning
    Adjusted flex styling in Shopping Cart
    Removed missing DataTables function error
    Changed files from 4 space indents to 2 spaces
    Added .min.js files from previous commit

--- gallery/static/gallery/js/gallery.js
+++ gallery/static/gallery/js/gallery.js
@@ -1,75 +1,77 @@
 $(document).ready(function () {
-    let table = $('.orders').DataTable();
+  if (typeof DataTable !== 'undefined') {
+    var table = $('.orders').DataTable();
+  }
 
-    $('.orders tbody tr').click(function (e) {
-        if (e.target.type == "checkbox") {
-            e.stopPropagation();
+  $('.orders tbody tr').click(function (e) {
+    if (e.target.type == "checkbox") {
+      e.stopPropagation();
+    } else {
+      window.location = '/gallery/order?id=' + $(this).data('id');
+    }
+  });
+
+  $('#pending-shipped').click(function () {
+    let orders = [];
+    $('#pending input:checked').each(function () {
+      orders.push($(this).parent().parent().data('id'));
+    });
+
+    $.ajax({
+      url: "/gallery/mark_shipped",
+      traditional: true,
+      data: {
+        'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
+        'orders': orders,
+      },
+      type: 'POST',
+      success: function (data) {
+        if (data.success) {
+          location.reload();
         } else {
-            window.location = '/gallery/order?id=' + $(this).data('id');
+          console.log('There was an error.');
         }
+      }
+    });
+  });
+
+  $('#pending-delete').click(function () {
+    let orders = [];
+    $('#pending input:checked').each(function () {
+      orders.push($(this).parent().parent().data('id'));
     });
 
-    $('#pending-shipped').click(function () {
-        let orders = [];
-        $('#pending input:checked').each(function () {
-            orders.push($(this).parent().parent().data('id'));
-        });
+    $.ajax({
+      url: "/gallery/delete_sales",
+      traditional: true,
+      data: {
+        'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
+        'orders': orders,
+      },
+      type: 'POST',
+      success: function () {
+        location.reload();
+      }
+    });
+  });
 
-        $.ajax({
-            url: "/gallery/mark_shipped",
-            traditional: true,
-            data: {
-                'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
-                'orders': orders,
-            },
-            type: 'POST',
-            success: function (data) {
-                if (data.success) {
-                    location.reload();
-                } else {
-                    console.log('There was an error.');
-                }
-            }
-        });
+  $('#shipped-delete').click(function () {
+    let orders = [];
+    $('#shipped input:checked').each(function () {
+      orders.push($(this).parent().parent().data('id'));
     });
 
-    $('#pending-delete').click(function () {
-        let orders = [];
-        $('#pending input:checked').each(function () {
-            orders.push($(this).parent().parent().data('id'));
-        });
-
-        $.ajax({
-            url: "/gallery/delete_sales",
-            traditional: true,
-            data: {
-                'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
-                'orders': orders,
-            },
-            type: 'POST',
-            success: function () {
-                location.reload();
-            }
-        });
+    $.ajax({
+      url: "/gallery/delete_sales",
+      traditional: true,
+      data: {
+        'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
+        'orders': orders,
+      },
+      type: 'POST',
+      success: function () {
+        location.reload();
+      }
     });
-
-    $('#shipped-delete').click(function () {
-        let orders = [];
-        $('#shipped input:checked').each(function () {
-            orders.push($(this).parent().parent().data('id'));
-        });
-
-        $.ajax({
-            url: "/gallery/delete_sales",
-            traditional: true,
-            data: {
-                'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
-                'orders': orders,
-            },
-            type: 'POST',
-            success: function () {
-                location.reload();
-            }
-        });
-    });
+  });
 });
