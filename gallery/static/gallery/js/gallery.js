1a0988211bff3a260ff51714adccf793657ac2c2
Matt McCarthy <matt@mccarthycode.com>
2020-12-06 17:45:58 -0600

    Fix momentum scrolling in Gallery page

--- gallery/static/gallery/js/gallery.js
+++ gallery/static/gallery/js/gallery.js
@@ -1,28 +1,44 @@
-$(document).ready(function () {
+$(() => {
   if (typeof DataTable !== 'undefined') {
     var table = $('.orders').DataTable();
   }
 
-  $('.orders tbody tr').click(function (e) {
-    if (e.target.type == "checkbox") {
+  $('html').css({ 'scrollbar-width': 'none' });
+  $('.nav-placeholder').remove();
+
+  const update = () => {
+    const windowDims = {
+      width: $(window).width(),
+      height: $(window).height(),
+    };
+    const navHeight = windowDims.width < 768 ? 47 : 56;
+
+    $('html').css({ height: `${windowDims.height}px` });
+    $('#scroll').css({ height: `${windowDims.height - navHeight}px` });
+  };
+  update();
+  $(window).on('resize orientationchange', update);
+
+  $('.orders tbody tr').on('click', function (e) {
+    if (e.target.type == 'checkbox') {
       e.stopPropagation();
     } else {
       window.location = '/gallery/order?id=' + $(this).data('id');
     }
   });
 
-  $('#pending-shipped').click(function () {
+  $('#pending-shipped').on('click', function () {
     let orders = [];
     $('#pending input:checked').each(function () {
       orders.push($(this).parent().parent().data('id'));
     });
 
     $.ajax({
-      url: "/gallery/mark_shipped",
+      url: '/gallery/mark_shipped',
       traditional: true,
       data: {
-        'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
-        'orders': orders,
+        csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
+        orders: orders,
       },
       type: 'POST',
       success: function (data) {
@@ -31,47 +47,47 @@ $(document).ready(function () {
         } else {
           console.log('There was an error.');
         }
-      }
+      },
     });
   });
 
-  $('#pending-delete').click(function () {
+  $('#pending-delete').on('click', function () {
     let orders = [];
     $('#pending input:checked').each(function () {
       orders.push($(this).parent().parent().data('id'));
     });
 
     $.ajax({
-      url: "/gallery/delete_sales",
+      url: '/gallery/delete_sales',
       traditional: true,
       data: {
-        'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
-        'orders': orders,
+        csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
+        orders: orders,
       },
       type: 'POST',
       success: function () {
         location.reload();
-      }
+      },
     });
   });
 
-  $('#shipped-delete').click(function () {
+  $('#shipped-delete').on('click', function () {
     let orders = [];
     $('#shipped input:checked').each(function () {
       orders.push($(this).parent().parent().data('id'));
     });
 
     $.ajax({
-      url: "/gallery/delete_sales",
+      url: '/gallery/delete_sales',
       traditional: true,
       data: {
-        'csrfmiddlewaretoken': $('input[name=csrfmiddlewaretoken]').val(),
-        'orders': orders,
+        csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
+        orders: orders,
       },
       type: 'POST',
       success: function () {
         location.reload();
-      }
+      },
     });
   });
 });
