00602d73e511e7ac85ad0cee060c6e8201fed9b2
Matt McCarthy <matt@mccarthycode.com>
2020-12-15 05:47:24 -0600

    Edit about.js
    - Update formatting, constants and variables, function declarations, and
      comments
    - Replace previously used element queries with newly implemented
      elements
    - Update deprecated jQuery function calls

--- about/static/about/js/about.js
+++ about/static/about/js/about.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(() => {
   // show appropriate view based on document hash
   if (document.location.hash) {
     setTimeout(function () {
@@ -6,9 +6,11 @@ $(document).ready(function () {
     }, 1);
   }
 
-  if (document.location.hash === '' ||
+  if (
+    document.location.hash === '' ||
     document.location.hash === '#' ||
-    document.location.hash === '#bio') {
+    document.location.hash === '#bio'
+  ) {
     $('#bio').show();
     $('#tabs-bio').addClass('active');
   } else if (document.location.hash === '#updates') {
@@ -19,27 +21,34 @@ $(document).ready(function () {
     $('#tabs-contact').addClass('active');
   }
 
-  // infinite scrolling and scroll to top
+  // define useful variables and constants
+  const $window = $(window);
+  const $scrollToTop = $('.scroll-to-top');
+  const $content = $('#content');
+
   let updatesPage = 2;
   let updatesEmpty = false;
-  let $scrollToTop = $('.scroll-to-top');
   let $lastUpdate;
 
-  $(window).on('resize scroll', function () {
+  // infinite scrolling and scroll to top
+  function update() {
     // infinite scrolling
-    $lastUpdate = $('#updates ul').find('li:last-of-type');
+    $lastUpdate = $('#updates > ul > li:last-of-type');
 
     if (!updatesEmpty && $lastUpdate.isInViewport()) {
       getPosts();
     }
 
     // scroll to top display
-    if ($(window).scrollTop() >= 180) {
+    if ($(this).scrollTop() >= 180) {
       $scrollToTop.slideDown();
     } else {
       $scrollToTop.slideUp();
     }
-  });
+  }
+
+  $window.on('resize', update);
+  $content.on('scroll', update);
 
   // determine if element is in viewport
   $.fn.isInViewport = function () {
@@ -47,10 +56,10 @@ $(document).ready(function () {
       return false;
     }
 
-    let elementTop = $(this).offset().top;
-    let elementBottom = elementTop + $(this).outerHeight();
-    let viewportTop = $(window).scrollTop();
-    let viewportBottom = viewportTop + $(window).height();
+    const elementTop = $(this).offset().top;
+    const elementBottom = elementTop + $(this).outerHeight();
+    const viewportTop = $window.scrollTop();
+    const viewportBottom = viewportTop + $window.height();
 
     return elementBottom >= viewportTop && elementTop <= viewportBottom;
   };
@@ -62,7 +71,7 @@ $(document).ready(function () {
       page: updatesPage++,
     };
 
-    beforeAjaxPosition = $(window).scrollTop();
+    beforeAjaxPosition = $window.scrollTop();
 
     $.get('/about/get_posts', data, function (response) {
       if (response === undefined) {
@@ -71,78 +80,82 @@ $(document).ready(function () {
       }
 
       $('#updates ul').append(response);
-      $(window).scrollTop(beforeAjaxPosition);
-      $lastUpdate = $('#updates ul').find('li:last-of-type');
+      $window.scrollTop(beforeAjaxPosition);
+      $lastUpdate = $('#updates > ul > li:last-of-type');
     });
   }
 
   // prevent tab, filter, filter control, and scroll to top behavior on scroll
   $(document).on('touchstart', function () {
-    startPosition = $(window).scrollTop();
+    startPosition = $window.scrollTop();
   });
 
   // scroll to top event handling
-  $(document).on('click touchstart', '.scroll-to-top', function (event) {
-    $("html, body").animate({
-      scrollTop: 0,
-    }, 1000);
+  $scrollToTop.on('click touchstart', function () {
+    $content.animate(
+      {
+        scrollTop: 0,
+      },
+      1000,
+    );
   });
 
   // define tabs behavior
-  let $activeTab = $('ul.tabs > li.active');
-  let $tabs = $('ul.tabs');
-  let $tabsChildren = $('ul.tabs > li');
+  const $tabs = $('ul.tabs');
+  const $tabsChildren = $tabs.children('li');
 
-  $tabsChildren.on('click touchstart', function (event) {
-    event.preventDefault();
-    event.stopPropagation();
+  let $activeTab = $tabs.children('li.active');
 
-    $tabsChildren.removeClass('active');
-
-    if (event.type === 'click') {
-      $tabsChildren
-        .removeClass('active')
-        .each(function () {
-          $($(this).data('href')).hide();
-        });
-
-      $activeTab = $(this);
-      $($(this).data('href')).show();
-    } else {
-      $(this).addClass('active');
-    }
-  });
-
-  $tabsChildren.on('touchend', function (event) {
-    let changes = event.changedTouches[0];
-    let $endElement = $(document.elementFromPoint(changes.pageX, changes.pageY));
-
-    if ($endElement.parent('.tabs').length) {
-      $tabsChildren
-        .removeClass('active')
-        .each(function () {
-          $($(this).data('href')).hide();
-        });
-
-      $activeTab = $endElement;
-      $endElement.addClass('active');
-      $($endElement.data('href')).show();
+  $tabsChildren
+    .on('click touchstart', function (event) {
+      event.preventDefault();
       event.stopPropagation();
-    }
-  });
 
-  $tabs.mouseenter(function () {
-    $activeTab.removeClass('active');
-  });
-  $tabs.mouseleave(function () {
-    $activeTab.addClass('active');
-  });
+      $tabsChildren.removeClass('active');
 
-  $(window).on('touchstart', function () {
-    $activeTab.addClass('active');
-  });
-  $(window).on('touchend', function () {
-    $tabsChildren.removeClass('active');
-    $activeTab.addClass('active');
-  });
+      if (event.type === 'click') {
+        $tabsChildren.removeClass('active').each(function () {
+          $($(this).data('href')).hide();
+        });
+
+        $activeTab = $(this);
+        $($(this).data('href')).show();
+      } else {
+        $(this).addClass('active');
+      }
+    })
+    .on('touchend', function (event) {
+      let changes = event.changedTouches[0];
+      let $endElement = $(
+        document.elementFromPoint(changes.pageX, changes.pageY),
+      );
+
+      if ($endElement.parent('.tabs').length) {
+        $tabsChildren.removeClass('active').each(function () {
+          $($(this).data('href')).hide();
+        });
+
+        $activeTab = $endElement;
+        $endElement.addClass('active');
+        $($endElement.data('href')).show();
+        event.stopPropagation();
+      }
+    });
+
+  $tabs
+    .on('mouseenter', () => {
+      $activeTab.removeClass('active');
+    })
+    .on('mouseleave', () => {
+      $activeTab.addClass('active');
+    });
+
+  $window
+    .on('touchstart', () => {
+      $activeTab.addClass('active');
+    })
+    .on('touchend', () => {
+      $tabsChildren.removeClass('active');
+      $activeTab.addClass('active');
+    });
 });
